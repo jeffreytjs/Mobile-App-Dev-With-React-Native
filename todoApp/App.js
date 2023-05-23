bd278c4dfc6af7db880b9e6196fc1cfe09e26044
jeffreytjs <jeffreytjs1996@gmail.com>
2023-05-23 21:24:32 +0800

    Add textInput field and styling

--- todoApp/App.js
+++ todoApp/App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Todo Entry:</Text>
+      <TextInput style={styles.textInput} />
+      <Button title="Add" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    width: "90%",
+    height: 40,
+    padding: 5,
   },
 });
