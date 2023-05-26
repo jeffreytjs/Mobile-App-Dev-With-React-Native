bb25d8baa6d2ebb25e5646ec51ea1843fc496947
jeffreytjs <jeffreytjs1996@gmail.com>
2023-05-26 18:43:39 +0800

    Prep for dark mode enhancement

--- crmApp/config.js
+++ crmApp/config.js
@@ -1,11 +1,21 @@
+import { Appearance } from "react-native";
+
 const config = {
+  PRIMARY_COLOR: (function() {
+    if (Appearance.getColorScheme() === "dark") {
+      return "black";
+    } else {
+      return "#229ED9";
+    }
+  })(),
+  HEADER_COLOR: "#229ED9",
+
   TITLE: "CRM App",
   MAIN_SCREEN_TITLE: "CRM App",
   ADD_CUSTOMER_SCREEN_TITLE: "Add Customer",
   LISTING_SCREEN: "ListingScreen",
   INPUT_SCREEN: "InputScreen",
   DATA_KEY: "TODOAPP_DATA",
-  HEADER_COLOR: "#229ED9",
 
   UNABLE_TO_ADD_CUSTOMER: "Unable to add customer. Please try again.",
   UNABLE_TO_LOAD_CUSTOMER:
